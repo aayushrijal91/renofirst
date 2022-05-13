0175e002a9ed79e747324f5e3c8f6108b47a5a64
Aayush Rijal <aayush.rijal99@gmail.com>
2022-05-13 14:59:06 +1000

    Project initialized

---
+++ README.md
@@ -0,0 +1,23 @@
+# Landing page [Reno First](http://renofirst.com.au/)
+
+This is a landing page boilerplate that uses PHP scripting language
+
+## _Author Information_
+
+Developer: Aayush Rijal
+
+Email: arijal@aiims.com.au, aayush.rijal99@gmail.com
+
+Contact: 0452637371
+
+[Github](https://www.figma.com/file/IBRR17Lg38hE1XwBQoQHee/Reno-First?node-id=0%3A1)
+[Figma](https://github.com/aayushrijal91/renofirst)
+
+## _Instruction_
+
+```bash
+ npm install
+ npm start
+ ```
+
+Reference for full repository
---
+++ app/functions.php
@@ -0,0 +1,20 @@
+<?php
+$site = "Website Name";
+$phone_number = "0432 406 340";
+$admin_email = 'arijal@aiims.com.au';
+$bcc_email = "";
+$no_reply_email = 'no-reply@icfitouts.com.au';
+$recaptcha_client_secret = '6LeSsm8eAAAAABTDNbqczWmMgvjWjRMmpFPQQnxH';
+$recaptcha_server_secret = '6LeSsm8eAAAAAMOIvIQQ43lCdFz_wswrwZgqNKF2';
+
+function renderImg($filename, $folder)
+{
+    $filename_without_ext = preg_replace('/\\.[^.\\s]{3,4}$/', '', $filename);
+    $src = "./assets/images/" . $folder . "/" . $filename;
+
+    if (file_exists("./assets/images/" . $folder . "/" . $filename_without_ext . ".webp")) {
+        $src = "./assets/images/" . $folder . "/" . $filename_without_ext . ".webp";
+    }
+
+    return "<img src=" . $src . " alt=" . $filename_without_ext . ">";
+}
