a6a3ee65b81de35e5d42454ee6c6f5bc60bd3771
Aayush Rijal <aayush.rijal99@gmail.com>
2022-05-19 23:52:40 +1000

    WIP: service

--- app/src/main.php
+++ app/src/main.php
@@ -18,6 +18,146 @@ include __DIR__ . '/header.php';
 	</div>
 </section>
 
+<section class="services">
+	<div class="container">
+		<div class="h5 fw-700 text-center line-height-1">Our RenoFirst Services</div>
+		<div class="subHeading">
+			Sometimes, it doesn't take much to spruce up the appearance of a dated or old bathroom. Today, many different modern<br class="d-none d-xxl-block"> plumbing fixtures and fittings can uplift your bathroom's functionality, water efficiency, energy efficiency, and look. We know what it takes to handle bathroom renovations, and our team can:
+		</div>
+	</div>
+</section>
+
+<section class="energy-saving">
+	<div class="container-fluid px-0">
+		<div class="row no-gutters align-items-center">
+			<div class="col-12 col-lg-auto">
+				<?= renderImg("energy-saving.jpg", "lib", "hero-img") ?>
+			</div>
+			<div class="col-12 col-lg description">
+				<div class="text-white fw-700 line-height-3 h5">Kitchen Renovations</div>
+				<div class="text-white h13 fw-600 pt-5">Like bathrooms, kitchens also need to be renovated sometimes. The plumbing features see a significant amount of wear due to daily use. As part of our kitchen renovation solutions, we can:</div>
+				<div class="horizontal-divider"></div>
+				<div class="d-flex align-items-center text-white fw-700 h11"><?= renderImg("tick-primary.png", "icons", "pr-3") ?>Replace kitchen faucets</div>
+				<div class="d-flex align-items-center text-white fw-700 h11"><?= renderImg("tick-primary.png", "icons", "pr-3") ?>Relocate the kitchen sink</div>
+				<div class="d-flex align-items-center text-white fw-700 h11"><?= renderImg("tick-primary.png", "icons", "pr-3") ?>Relocate kitchen appliances and the connected plumbing</div>
+				<div class="d-flex align-items-center text-white fw-700 h11"><?= renderImg("tick-primary.png", "icons", "pr-3") ?>Other</div>
+				<div class="text-dark-grey font-roboto h15 pt-4">
+					We are very systematic in the tasks we handle and ensure that every stage of the project is planned perfectly down to the last detail. Our designers have detailed discussions to understand what your ideas and requirements are before recommending the best solutions. We also provide you with all the information you need about various plumbing products and recommend ones that would be best suited to your needs.
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="benefits">
+	<div class="container">
+		<div class="h5 fw-700 text-center line-height-1">ADA Accessibility Upgrades</div>
+		<div class="description">
+			Bathrooms can be pretty unsafe for the elderly or people with mobility issues. The water and smooth tiles can become a slipping hazard.<br>
+			Without hand bars and ergonomic amenities, it can be incredibly uncomfortable, inconvenient and unsafe for them to use their bathrooms every day. We handle ADA accessibility upgrades and can add easy-turn faucets, grab rails.
+		</div>
+		<div class="benefits-card">
+			<div class="row justify-content-between align-items-center">
+				<div class="col-auto text-italic line-height-1 h9 fw-700">Kitchen Remodelling<br> Services</div>
+				<div class="col-7 font-roboto text-dark-grey">
+					<div class="pb-3">We handle kitchen remodels with as much creativity and skill as bathroom projects. Our team can replace or add:</div>
+					<div class="row no-gutters justify-content-between pt-1">
+						<div class="col-auto d-flex align-items-center"><?= renderImg("tick-primary.png", "icons", "pr-3") ?> Kitchen Faucets &amp; Sinks</div>
+						<div class="col-auto d-flex align-items-center px-1"><?= renderImg("tick-primary.png", "icons", "pr-3") ?> Plumbing Lines For Appliances</div>
+						<div class="col-auto d-flex align-items-center px-1"><?= renderImg("tick-primary.png", "icons", "pr-3") ?> Water Plumbing Lines</div>
+						<div class="col-auto d-flex align-items-center"><?= renderImg("tick-primary.png", "icons", "pr-3") ?> Other</div>
+					</div>
+				</div>
+				<div class="col-auto"><?= renderImg("green-tick.png", "icons") ?></div>
+			</div>
+		</div>
+		<div class="benefits-card">
+			<div class="row justify-content-between align-items-center">
+				<div class="col-auto text-italic line-height-1 h9 fw-700">100% Customer<br> Satisfaction Guarantee</div>
+				<div class="col-7 font-roboto text-dark-grey">
+					We are a skilled bathroom and kitchen remodelling company that offers high-grade solutions at cost-effective price points. Our experts will visit your property and assess the existing features' condition before recommending the best solutions, within your budget. We source high-grade materials and products from credible local suppliers, complete every task to industry standards, covering all our work with guarantees, and finish the job on time.
+				</div>
+				<div class="col-auto"><?= renderImg("green-tick.png", "icons") ?></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="why-us">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-5 px-0">
+				<?= renderImg("why-us.png", "lib", "cover-img") ?>
+			</div>
+			<div class="col-7 px-6 py-4">
+				<div class="h5 fw-700 line-height-3 text-white">Cost-Effective Bathroom &amp; Kitchen Renovations</div>
+				<div class="horizontal-divider"></div>
+				<div class="h15 text-secondary font-roboto">
+					<p>We have years of experience in handling these projects, and you can be sure that your bathroom or kitchen will be more comfortable, functional and attractive after the renovation. We also provide our clients excellent value for money by delivering top-notch services within their budget. We cover all our work with iron-clad guarantees, and you can be sure that we will handle the job right the first time around.</p>
+					<p>For more information about our bathroom & kitchen renovations, feel free to call RenoFirst at <a href="tel:<?= $phone_number ?>"><?= $phone_number ?></a> or send us a message through this <a href="#form">Online Form</a> today, and one of our team members will contact you to discuss your requirements.</p>
+				</div>
+				<div class="d-flex align-items-center text-white h11 fw-700 pt-4 pb-3">
+					<?= renderImg("tick-primary.png", "icons", "pr-2") ?>Why Choose Us
+				</div>
+				<div class="h15 text-secondary font-roboto">
+					<p>When you choose RenoFirst for your plumbing, hot water and drain needs across Sydney, you can be sure that our high quality, friendly service is second-to-none. Discover the RenoFirst difference for yourself, or read from our other happy customers here.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="cta">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 col-lg-3">
+				<div class="cta-card">
+					<?= renderImg("cta-1.jpg", "lib", "cta_image") ?>
+					<div class="card-body">
+						<div class="card-head">On-time... Guaranteed</div>
+						<div class="card-text">
+							With RenoFirst you can be so sure of on-time service that we even promise that if we're late, we'll pay you*. It's your assurance of punctual, convenient plumbers that work around your schedule.
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<div class="cta-card">
+					<?= renderImg("cta-2.jpg", "lib", "cta_image") ?>
+					<div class="card-body">
+						<div class="card-head">Faster Service</div>
+						<div class="card-text">
+							Because RenoFirst operates from MULTIPLE locations right across the Sydney metropolitan area, you can benefit from a rapid response for your plumbing needs.
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<div class="cta-card">
+					<?= renderImg("cta-3.jpg", "lib", "cta_image") ?>
+					<div class="card-body">
+						<div class="card-head">AY BY THE JOB NOT THE HOUR</div>
+						<div class="card-text">
+							With RenoFirst you always know the price BEFORE we start, for your complete peace of mind. And because you pay by the job, and not by the hour, no matter how long the job takes you still only pay the same price you agreed, which can help save you money.
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<div class="cta-card">
+					<?= renderImg("cta-4.jpg", "lib", "cta_image") ?>
+					<div class="card-body">
+						<div class="card-head">friendly Plumbers</div>
+						<div class="card-text">
+							Courteous, polite and friendly plumbers that will take the time to answer all of your questions and help with all your plumbing requirements. It's the little details that can make all the difference.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <section class="form" id="form">
 	<div class="container">
 		<div class="form_container">
